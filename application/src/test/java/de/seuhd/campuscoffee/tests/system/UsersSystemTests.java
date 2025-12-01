2f2be65b34ba08c4c62fc218a4cb1ef698431b5b
Lasse Schwabedissen <Lasse.schwabedissen@gmail.com>
2025-12-01 21:07:41 +0100

    2 Added Tests

--- application/src/test/java/de/seuhd/campuscoffee/tests/system/UsersSystemTests.java
+++ application/src/test/java/de/seuhd/campuscoffee/tests/system/UsersSystemTests.java
@@ -3,10 +3,14 @@ package de.seuhd.campuscoffee.tests.system;
 import de.seuhd.campuscoffee.domain.model.User;
 import de.seuhd.campuscoffee.domain.tests.TestFixtures;
 import org.junit.jupiter.api.Test;
-
+import de.seuhd.campuscoffee.api.dtos.UserDto;
 import java.util.List;
-
 import static de.seuhd.campuscoffee.tests.SystemTestUtils.Requests.userRequests;
+import static org.junit.Assert.assertEquals;
+
+
+
+
 
 public class UsersSystemTests extends AbstractSysTest {
 
@@ -18,6 +22,23 @@ public class UsersSystemTests extends AbstractSysTest {
         User createdUser = userDtoMapper.toDomain(userRequests.create(List.of(userDtoMapper.fromDomain(userToCreate))).getFirst());
 
         assertEqualsIgnoringIdAndTimestamps(createdUser, userToCreate);
+    }
+
+
+    @Test
+    void getAllUsers() {
+        List<User> created = TestFixtures.createUsers(userService);
+        List<UserDto> response = userRequests.retrieveAll();
+        assertEquals(created.size(), response.size());
+    }
+
+    @Test
+    void getUserById() {
+        List<User> createdUsers = TestFixtures.createUsers(userService);
+        User expected = createdUsers.getFirst();
+        UserDto response = userRequests.retrieveById(expected.id());
+        User returned = userDtoMapper.toDomain(response);
+        assertEqualsIgnoringIdAndTimestamps(returned, expected);
     }
 
     //TODO: Add at least two additional tests for user operations
